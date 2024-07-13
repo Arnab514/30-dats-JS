2e6882166adefd59926341dc3a86e4bbb105fb6e
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-07-13 21:34:08 +0530

    Day 1

---
+++ Day 1(Varriables and Data types)/index.js
@@ -0,0 +1,48 @@
+//Activity - 1
+ //Task 1
+ var numberVar = 2
+ console.log(numberVar)
+
+ //Task 2 
+ let strLet = 'Arnab'
+ console.log(strLet)
+
+//Activity - 2
+ //Task 3
+ const boolConst = true
+ console.log(boolConst) 
+
+//Activity - 3
+ //Task 4
+ var numrVar = 1
+ let strVar = 'Arnab'
+ let boolVar = true
+ let objVar = {
+    name : "Arnab",
+    Degree : "B.tech"
+ }
+ let arrVar = [1 , 4 , 3 ,5]
+
+ console.log(typeof numberVar)
+ console.log(typeof strVar)
+ console.log(typeof boolVar)
+ console.log(typeof objVar)
+ console.log(typeof arrVar)
+
+//Activity - 3
+ //Task 5
+ let reassignVar = 10
+ console.log(reassignVar)
+ 
+ reassignVar = "JS"
+ console.log(reassignVar)
+
+
+
+ // Achievement:
+// By the end of these activities, you will:
+
+// Know how to declare variables using var, let, and const.
+// Understand the different data types in JavaScript.
+// Be able to use the typeof operator to identify the data type of a variable.
+// Understand the concept of variable reassignment and the immutability of const variables.
