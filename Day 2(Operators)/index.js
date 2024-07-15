3049ed88b41e6aa959f66311df4409a3a4e474d3
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-07-15 15:26:49 +0530

    Day 2 done

---
+++ Day 2(Operators)/index.js
@@ -0,0 +1,20 @@
+// Activity 1: Arithmetic Operations
+ // Task 1: Write a program to add two numbers and log the result to the console.
+ let num1 = 5;
+ let num2 = 3;
+ let sum = num1 + num2;
+ console.log("Sum:", sum); // Output: Sum: 8
+
+ // Task 2: Write a program to subtract two numbers and log the result to the console.
+ let difference = num1 - num2;
+ console.log("Difference:", difference); // Output: Difference: 2
+
+ // Task 3: Write a program to multiply two numbers and log the result to the console.
+ let product = num1 * num2;
+ console.log("Product:", product); // Output: Product: 15
+
+ // Task 4: Write a program to divide two numbers and log the result to the console.
+ let quotient = num1 / num2;
+ console.log("Quotient:", quotient); // Output: Quotient: 1.6666666666666667
+ 
+ 
