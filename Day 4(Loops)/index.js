50ecac1d0813bfb2b4a7eb663d322fb8962c4f1d
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-07-28 03:01:58 +0530

    Day 4

---
+++ Day 4(Loops)/index.js
@@ -0,0 +1,21 @@
+// Activity 1: For Loop
+ // Task 1: Print numbers from 1 to 10 using a for loop.
+    for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    }
+
+// Task 2: Print the multiplication table of 5 using a for loop.
+    for (let i = 1; i <= 10; i++) {
+        console.log(`5 x ${i} = ${5 * i}`);
+    }
+
+// Activity 2: While Loop
+// Task 3: Calculate the sum of numbers from 1 to 10 using a while loop.
+    let sum = 0;
+    let i = 1;
+    while (i <= 10) {
+    sum += i;
+    i++;
+    }
+    console.log(`Sum of numbers from 1 to 10 is ${sum}`);
+
